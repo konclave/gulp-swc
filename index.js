5fc871ed6ffa0bbdc22664e66dfda998c4aa9766
Ivan Vasilyev <vianomore@gmail.com>
2020-11-05 00:23:19 +0300

    refactor: replace through2 with node stream

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-const swc = require("@swc/core");
-const path = require("path");
-const replaceExt = require("replace-ext");
-const through = require("through2");
-const PluginError = require("plugin-error");
-const applySourceMap = require("vinyl-sourcemaps-apply");
+const swc = require('@swc/core');
+const path = require('path');
+const replaceExt = require('replace-ext');
+const PluginError = require('plugin-error');
+const applySourceMap = require('vinyl-sourcemaps-apply');
+const { Transform } = require('readable-stream');
 
 function replaceExtension(fp) {
-  return path.extname(fp) ? replaceExt(fp, ".js") : fp;
+  return path.extname(fp) ? replaceExt(fp, '.js') : fp;
 }
 
 module.exports = function (opts) {
   opts = opts || {};
 
-  return through.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      cb(null, file);
-      return;
-    }
+  return new Transform({
+    objectMode: true,
+    transform(file, enc, cb) {
+      if (file.isNull()) {
+        cb(null, file);
+        return;
+      }
 
-    if (file.isStream()) {
-      cb(new PluginError("gulp-swc", "Streaming not supported"));
-      return;
-    }
+      if (file.isStream()) {
+        cb(new PluginError('gulp-swc', 'Streaming not supported'));
+        return;
+      }
 
-    const fileOpts = Object.assign({}, opts, {
-      filename: file.path,
-      sourceMaps: Boolean(file.sourceMap),
-      caller: Object.assign(
-				{name: 'gulp-swc'},
-				opts.caller
-			)
-    });
+      const fileOpts = Object.assign({}, opts, {
+        filename: file.path,
+        sourceMaps: Boolean(file.sourceMap),
+        caller: Object.assign({ name: 'gulp-swc' }, opts.caller),
+      });
 
-    swc
-      .transform(file.contents.toString(), fileOpts)
-      .then((res) => {
-        if (res) {
-          if (file.sourceMap && res.map) {
-            res.map.file = replaceExtension(file.relative);
-            applySourceMap(file, res.map);
+      swc
+        .transform(file.contents.toString(), fileOpts)
+        .then((res) => {
+          if (res) {
+            if (file.sourceMap && res.map) {
+              res.map.file = replaceExtension(file.relative);
+              applySourceMap(file, res.map);
+            }
+
+            file.contents = Buffer.from(res.code);
+            file.path = replaceExtension(file.path);
           }
 
-          file.contents = Buffer.from(res.code);
-          file.path = replaceExtension(file.path);
-        }
-
-        this.push(file);
-      })
-      .catch((error) => {
-        this.emit(
-          "error",
-          new PluginError("gulp-swc", error, {
-            fileName: file.path,
-            showProperties: false,
-          })
+          this.push(file);
+        })
+        .catch((error) => {
+          this.emit(
+            'error',
+            new PluginError('gulp-swc', error, {
+              fileName: file.path,
+              showProperties: false,
+            })
+          );
+        })
+        .then(
+          () => cb(),
+          () => cb()
         );
-      })
-      .then(
-        () => cb(),
-        () => cb()
-      );
+    },
   });
 };
